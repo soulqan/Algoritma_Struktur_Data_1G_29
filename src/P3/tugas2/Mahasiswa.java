4e5035a8dfad9e2bce14b3eca743226e898cdcb8
soulqan <soultan.agnar@gmail.com>
2024-02-28 17:16:06 +0700

    P3_Tugas2&3_29

---
+++ src/P3/tugas2/Mahasiswa.java
@@ -0,0 +1,33 @@
+package P3.tugas2;
+
+class Mahasiswa {
+    String nama;
+    String nim;
+    char jenisKelamin;
+    double ipk;
+
+    // Constructor
+    public Mahasiswa(String nama, String nim, char jenisKelamin, double ipk) {
+        this.nama = nama;
+        this.nim = nim;
+        this.jenisKelamin = jenisKelamin;
+        this.ipk = ipk;
+    }
+
+    // Getter methods
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public char getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public double getIpk() {
+        return ipk;
+    }
+}
