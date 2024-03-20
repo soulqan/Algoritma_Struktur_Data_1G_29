e8e9cf7786bee214ed0858aa49b9e2734249ddaa
soulqan <soultan.agnar@gmail.com>
2024-03-20 20:31:02 +0700

    P5_Percobaan1_28

--- src/P2/BukuMain29.java
+++ src/P2/BukuMain29.java
@@ -4,7 +4,9 @@ public class BukuMain29 {
     public static void main(String[] args) {
         Buku29 bk1=new Buku29();
         bk1.judul= "Today Ends Tomorrow Comes";
+        
         bk1.pengarang= "Denanda Pratiwi";
+        bk1.pengarang ="John Irving";
         bk1.halaman=198;
         bk1.stok=13;
         bk1.harga=71000;
@@ -13,6 +15,7 @@ public class BukuMain29 {
         bk1.terjual(5);
         bk1.gantiHarga(60000);
         bk1.tampilInformasi();
+        
 
         Buku29 bk2 = new Buku29("Self Reward", "Maheera Ayesha", 160, 29, 59000,8);
         bk2.tampilInformasi();
--- src/P3/tugas2/Mahasiswa.java
+++ src/P3/tugas2/Mahasiswa.java
@@ -1,10 +1,13 @@
 package P3.tugas2;
 
+import java.util.Scanner;
+
 class Mahasiswa {
     String nama;
     String nim;
     char jenisKelamin;
     double ipk;
+    public static Scanner scanner = new Scanner(System.in);
 
     // Constructor
     public Mahasiswa(String nama, String nim, char jenisKelamin, double ipk) {
@@ -30,4 +33,62 @@ class Mahasiswa {
     public double getIpk() {
         return ipk;
     }
+    public static double getTotalIpk(Mahasiswa[] mahasiswas) {
+        double totalIpk = 0;
+        for (Mahasiswa mahasiswa : mahasiswas) {
+            totalIpk += mahasiswa.getIpk();
+        }
+        return totalIpk;
+    }
+
+    public static double getRataIpk(Mahasiswa[] mahasiswas) {
+        double totalIpk = getTotalIpk(mahasiswas);
+        return totalIpk / mahasiswas.length;
+    }
+
+    public static Mahasiswa getMahasiswaTertinggi(Mahasiswa[] mahasiswas) {
+        Mahasiswa mahasiswaTertinggi = mahasiswas[0];
+        for (int i = 1; i < mahasiswas.length; i++) {
+            if (mahasiswas[i].getIpk() > mahasiswaTertinggi.getIpk()) {
+                mahasiswaTertinggi = mahasiswas[i];
+            }
+        }
+        return mahasiswaTertinggi;
+    }
+    public static void printMahasiswaTertinggi(Mahasiswa[]mahasiswas){
+        Mahasiswa mahasiswaTertinggi = Mahasiswa.getMahasiswaTertinggi(mahasiswas);
+        System.out.println("\nMahasiswa dengan IPK tertinggi:");
+        System.out.println("Nama: " + mahasiswaTertinggi.getNama());
+        System.out.println("NIM: " + mahasiswaTertinggi.getNim());
+        System.out.println("Jenis Kelamin: " + mahasiswaTertinggi.getJenisKelamin());
+        System.out.println("IPK: " + mahasiswaTertinggi.getIpk());
+    }
+
+    public static void printData(Mahasiswa[]mahasiswas){
+        for (int i = 0; i < mahasiswas.length; i++) {
+            System.out.println("\nData Mahasiswa ke-" + (i+1));
+            System.out.println("Nama: " + mahasiswas[i].getNama());
+            System.out.println("NIM: " + mahasiswas[i].getNim());
+            System.out.println("Jenis Kelamin: " + mahasiswas[i].getJenisKelamin());
+            System.out.println("IPK: " + mahasiswas[i].getIpk());
+    }
 }
+    public static void input(Mahasiswa[]mahasiswas){
+        for (int i = 0; i < mahasiswas.length; i++) {
+            System.out.println("Masukkan data mahasiswa ke-" + (i+1));
+            System.out.print("Masukkan nama: ");
+            String nama = scanner.nextLine();
+            System.out.print("Masukkan nim: ");
+            String nim = scanner.nextLine();
+            System.out.print("Masukkan jenis kelamin (L/P): ");
+            char jenisKelamin = scanner.nextLine().charAt(0);
+            System.out.print("Masukkan IPK: ");
+            double ipk = scanner.nextDouble();
+            scanner.nextLine(); 
+
+            mahasiswas[i] = new Mahasiswa(nama, nim, jenisKelamin, ipk);
+        }
+    }
+}
+
+
