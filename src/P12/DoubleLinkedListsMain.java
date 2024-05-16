08ee3730d9e90fdc341a5bca09a03897f05b7534
soulqan <soultan.agnar@gmail.com>
2024-05-16 20:15:12 +0700

    P12_Percoaan3_28

--- src/P12/DoubleLinkedListsMain.java
+++ src/P12/DoubleLinkedListsMain.java
@@ -38,5 +38,22 @@ public class DoubleLinkedListsMain {
         dll.print();
         System.out.println("Size : " + dll.size);
         System.out.println("=========================================");
+        dll.removeLast();
+        dll.print();
+        System.out.println("Size : " + dll.size);
+        System.out.println("=========================================");
+        dll.addFirst(3);
+        dll.addLast(4);
+        dll.addFirst(7);
+        dll.print();
+        System.out.println("Size : " + dll.size);
+        System.out.println("=========================================");
+        dll.add(40, 1);
+        dll.print();
+        System.out.println("Size : " + dll.size);
+        System.out.println("=========================================");
+        System.out.println("Data awal pada Linked Lists adalah : " + dll.getFirst());
+        System.out.println("Data akhir pada Linked Lists adalah : " + dll.getLast());
+        System.out.println("Data indeks ke-1 pada Linked List adalah : " + dll.get(1));
     }
 }
