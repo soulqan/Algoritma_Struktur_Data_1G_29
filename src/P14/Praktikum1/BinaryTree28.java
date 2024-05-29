f5e2c81e336ba03df9f9ff242a357850a3d37164
soulqan <soultan.agnar@gmail.com>
2024-05-29 21:59:11 +0700

    P14_Percobaan2_28

--- src/P14/Praktikum1/BinaryTree28.java
+++ src/P14/Praktikum1/BinaryTree28.java
@@ -1,4 +1,4 @@
-package P14;
+package P14.Praktikum1;
 
 public class BinaryTree28 {
     Node28 root;
--- src/P14/Praktikum1/BinaryTreeMain28.java
+++ src/P14/Praktikum1/BinaryTreeMain28.java
@@ -1,4 +1,4 @@
-package P14;
+package P14.Praktikum1;
 
 public class BinaryTreeMain28 {
     public static void main(String[] args) {
