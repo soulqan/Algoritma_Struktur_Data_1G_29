a8bce9576835eadb3880e691c62bcf560a43dd28
soulqan <soultan.agnar@gmail.com>
2024-06-07 00:40:41 +0700

    P15_Percobaan2_28

--- src/P15/GraphMain28.java
+++ src/P15/GraphMain28.java
@@ -1,7 +1,10 @@
 package P15;
 
+import java.util.Scanner;
+
 public class GraphMain28 {
     public static void main(String[] args) throws Exception {
+        Scanner scanner28 = new Scanner(System.in);
         Graph28 gedung = new Graph28(6);
         gedung.addEdge(0, 1, 50);
         gedung.addEdge(0, 2, 100);
@@ -13,5 +16,17 @@ public class GraphMain28 {
         gedung.printGraph();
         gedung.removeEdge(1, 3);
         gedung.printGraph();
+
+        GraphMatriks28 gdg = new GraphMatriks28(4);
+        gdg.makeEdge(0, 1, 50);
+        gdg.makeEdge(1, 0, 60);
+        gdg.makeEdge(1, 2, 70);
+        gdg.makeEdge(2, 1, 80);
+        gdg.makeEdge(2, 3, 40);
+        gdg.makeEdge(3, 0, 90);
+        gdg.printGraph();
+        System.out.println("Hasil setelah penghapusan edge");
+        gdg.removeEdge(2, 1);
+        gdg.printGraph();
     }
 }
